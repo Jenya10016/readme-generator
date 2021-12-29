b3f58ed71ad542fd714ceef1dc7057e8d6603a21
Jenya.Seletsky <js646@me.com>
2021-12-28 19:36:02 -0500

    updated index questions

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 var inquirer = require('inquirer');
 inquirer
-  .prompt([
     /* Pass your questions in here */
+  .prompt([
+      {
+        type: 'input',
+        message: 'Add title of your project',
+        name: 'title',
+      },
+
+      {
+        type: 'input',
+        message: 'Please add a description of your project',
+        name: 'description',
+      },
+
+      {
+        type: 'input',
+        message: 'Please add installation instructions',
+        name: 'installation',
+      },
+
+      {
+        type: 'input',
+        message: 'Please add usage information',
+        name: 'usage',
+      },
+
+      {
+        type: 'input',
+        message: 'Please add contribution guidlines',
+        name: 'contribution',
+      },
+
+      {
+        type: 'input',
+        message: 'Please explain and add test instructions',
+        name: 'testInstructions',
+      },
+      
+      {
+        type: 'list',
+        message: 'Please choose your license',
+        choice: 'License',
+        name: 'license',
+      }
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
